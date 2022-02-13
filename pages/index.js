f6c3ff44615a42ad1b594f025a3aea5f126cd642
sava <savamedvedev@gmail.com>
2022-02-13 20:40:02 +0800

    first-commit

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+import { PostCard, PostWidget, Categories } from '../components'
+const posts = [
+  { title: 'React Home', excerpt: ' Learn react with tailwind' },
+  { title: 'React Homea', excerpt: ' Learn react with tailwinsd' },
+]
+
+export default function Home() {
+  return (
+    <div className="container mx-auto mb-8 px-10">
+      <Head>
+        <title>Next Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
+        <div className="col-span-1 lg:col-span-8">
+          {posts.map((post) => (
+            <PostCard post={post} key={post.title} />
+          ))}
+        </div>
+        <div className="col-span-1 lg:col-span-4">
+          <div className="relative top-8 lg:sticky">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
